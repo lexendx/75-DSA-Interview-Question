37c7fafa133e5c6c632fea8d4a55a2cb314cd304
Baibhav Tiwari <97496657+lexendx@users.noreply.github.com>
2024-12-28 18:28:17 +0530

    method of buy stocks

--- StockBuyAndSell.java
+++ StockBuyAndSell.java
@@ -4,7 +4,7 @@ public class StockBuyAndSell {
 
 
     public int maximumProfit(int arr[]) {
-        int profit=0;
+        int profit=;
         for(int i=1;i<arr.length;i++){
             if(arr[i]>arr[i-1]){
                 profit+=arr[i]-arr[i-1];
